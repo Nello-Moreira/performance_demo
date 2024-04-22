862c8ff3e9ec724ff70755df7df6d9ff9a9428d2
Nello-Moreira <g.moreira1@outlook.com>
2024-04-22 10:17:38 -0300

    Creates the docker image for the stress tests

---
+++ performance_tests/run_tests.sh
@@ -0,0 +1,43 @@
+sleep 10
+
+cat << EOF
+
+
+*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+Running stress tests for the loop version
+
+*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+
+EOF
+
+artillery run --output ./reports/loop_report.json ./tests/fibo_loop.yaml
+
+sleep 60
+
+cat << EOF
+
+
+*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+Running stress tests for the recursion version
+
+*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+
+EOF
+
+artillery run --output ./reports/recursion_report.json ./tests/fibo_recur.yaml
+
+cat << EOF
+
+
+*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+Stress tests finished
+
+*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+
+EOF
